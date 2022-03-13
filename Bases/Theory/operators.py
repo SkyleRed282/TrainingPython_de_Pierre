f169cf3216f21d9175340d1b903a4c9ad650e508
Pierre <pierre.anken@pm.me>
2022-03-13 20:06:59 +0100

    print

--- Bases/Theory/operators.py
+++ Bases/Theory/operators.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
         print('List is empty')
 
     # operateurs arithmetics
+    print(' == Operateurs arithmetics ==')
     var = 1 * 2 / 2 - 3 + 5
     print(5 % 2)
     print(5 // 2)
@@ -25,21 +26,23 @@ if __name__ == '__main__':
     var1 = var1 + 2
 
     # operateurs comparaison
+    print(' == operateurs de comparaison ==')
     print(1 == 2)
     print(1 != 2)
 
     # operateurs logiques
+    print(' == operateurs logiques ==')
     print(True and False)
     print(True or False)
     print(not False)
 
     # operateur identité
-    print(' == operateur d\'identé == ')
-    print(var1 is str)
+    print(' == operateur d\'identé ==')
+    print(type(var1) is int)
     print(isinstance(True, bool))
 
     # operateur d'appartenance
-    print(' == operateur d\'appartenance == ')
+    print(' == operateur d\'appartenance ==')
     print('x' in 'axc')
     print('x' in ['a', 'x', 'c'])
 
