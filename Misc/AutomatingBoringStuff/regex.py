0eb2954114f1232920a8fb8f02f9d08b5be1f315
Pierre Anken <pierre.anken@pm.me>
2022-08-24 14:44:03 +0200

    verbose correction

--- Misc/AutomatingBoringStuff/regex.py
+++ Misc/AutomatingBoringStuff/regex.py
@@ -138,12 +138,12 @@ if __name__ == '__main__':
     print(match_object)
 
     # verbose mode
-    phoneRegex = re.compile(r'''(
+    phoneRegex = re.compile(r'''
      (\d{3}|\(\d{3}\))?             # area code
      (\s|-|\.)?                     # separator
      \d{3}                          # first 3 digits
      (\s|-|\.)                      # separator
      \d{4}                          # last 4 digits
      (\s*(ext|x|ext.)\s*\d{2,5})?   # extension
-     )''', re.VERBOSE)
+     ''', re.VERBOSE)
 
