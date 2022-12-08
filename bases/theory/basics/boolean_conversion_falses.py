63e8751f9cc70805921b35e98ed6da96d0690b2e
Pierre Anken <pierre.anken@proton.me>
2022-12-08 18:04:54 +0100

    divers

--- bases/theory/basics/boolean_conversion_falses.py
+++ bases/theory/basics/boolean_conversion_falses.py
@@ -1,7 +1,7 @@
 false_values = [0, '', False, None, [], {}, tuple(), set(), 0.0]
 
 for false_value in false_values:
-    print(f'{false_value} of type {type(false_value).__name__} => {bool(false_value)}')
+    print(false_value, 'of type', type(false_value).__name__, ' =>', bool(false_value))
 
 my_list = []  # False
 if my_list:
--- bases/theory/basics/conditions.py
+++ bases/theory/basics/conditions.py
@@ -1,11 +1,16 @@
 if __name__ == '__main__':
 
     user_input = input('Enter a number ')
-    x = int(user_input)
 
-    if x > 150:
-        print("x > 150")
-    elif x < 150:
-        print("x < 150")
+    # Is the string only composed of integers?
+    if user_input.isdigit():
+        x = int(user_input)
+
+        if x > 150:
+            print("x > 150")
+        elif x < 150:
+            print("x < 150")
+        else:
+            print("x == 150")
     else:
-        print("x == 150")
+        print('Invalid number', user_input)
