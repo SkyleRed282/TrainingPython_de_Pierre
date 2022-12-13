0703fd18333776affc2e1a70991d2f5f28eb9680
Pierre Anken <pierre.anken@proton.me>
2022-12-13 17:01:40 +0100

    order course

--- bases/theory/basics/0_start_python_script.py
+++ bases/theory/basics/0_start_python_script.py
@@ -1,7 +1,6 @@
 # to start a Python script use 'python <path_to_script>'
 # python 0_start_python_script.py
-import string
 
 # Pycharm enable you to start it from a configuration file or a main
 if __name__ == '__main__':
-    print(string.ascii_lowercase)
+    print('Hello world')
---
+++ bases/theory/basics/3_debugging.py
@@ -0,0 +1,7 @@
+# breakpoints
+import random
+
+print('Something working')
+
+# code error
+print(random.randint(1, 2.3))
