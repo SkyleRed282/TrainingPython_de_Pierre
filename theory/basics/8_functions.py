e73fa8ccf5ac94abc3cd786cdbde11104918b72f
Pierre Anken <pierre.anken@proton.me>
2023-01-21 11:10:09 +0100

    cleanup fonctions

--- theory/basics/8_functions.py
+++ theory/basics/8_functions.py
@@ -1,120 +1,82 @@
-import datetime
 import random
 
 
-def say_hello(name: str = 'Lucas') -> None:
+def print_random_number_0_x(x: int) -> None:
     """
-    Say hello to with a given name
-    :param name: optional name from someone
-    :return: None
+    This function print a random number in range [0,x]
     """
-    print(f'Hello {name} ')
+    print('Print inside function:', random.sample(range(0, x), 1)[0])
 
 
-def ask_age() -> str:
+def print_random_number_0_9() -> None:
     """
-    Ask the user for his age and return it
+    This function print a random number in range [0,10]
     """
-    user_age = input('Age?')
-    return user_age
+    print_random_number_0_x(9)
 
 
-def say_hello_mp(name, age_user=25):
+def get_random_number_0_x(x: int) -> int:
     """
-    Say hello to with a given name and age
+    This function return a random number in range [0,x]
     """
-    print(f'Hello {name} {age_user}')
+    return random.sample(range(0, x), 1)[0]
 
 
-# *args and **kwargs are standard names for those special parameters
+def get_random_number_0_9() -> int:
+    """
+    This function return a random number in range [0,10]
+    """
+    return get_random_number_0_x(10)
+
+
+def get_random_number_0_x_optional(x: int = 20) -> int:
+    """
+    This function return a random number in range [0,x] or [0,20] if x is not given
+    """
+    return get_random_number_0_x(x)
+
+
+def get_random_value_in_values(*values) -> int | str:
+    """
+    This function return a random value from a list of parameters
+    """
+    return random.sample(values, 1)[0]
+
+
 def args_and_kwargs(*positional_arguments, **keyword_arguments):
     """
-    Print the type and the list of kwargs and args
+    Return the given arguments
     """
-    print(type(positional_arguments), positional_arguments)
-    print(type(keyword_arguments), keyword_arguments)
-
-
-def sum_example(*positional_arguments):
-    """
-    Return the sum of any number of positional arguments
-    """
-    result = 0
-    for param in positional_arguments:
-        result += param
-
-    return result
-
-
-def exemple_mix_positional(operation: str, *numbers):
-    if operation == '+':
-        print(sum(numbers))
-    else:
-        print(f'Operation {operation} non supportée')
-
-
-
-
-def list_shuffle(some_list: list) -> None:
-    """
-    This function shuffle a given list
-    """
-    # if some_list is not None and not empty
-    if some_list:
-        random.shuffle(some_list)
-
-
-def current_time() -> None:
-    """
-    This function print the current date time
-    """
-    print(datetime.datetime.now().isoformat())
-
-
-def current_time_iso() -> str:
-    """
-    This function return the current date time
-    """
-    return datetime.datetime.now().isoformat()
+    return [type(positional_arguments), positional_arguments, type(keyword_arguments), keyword_arguments]
 
 
 if __name__ == '__main__':
-
-    # No return no parameter
-    print("\nNo return no parameter")
-    print(current_time())
+    param = 20
 
     # No return, one parameter
-    print("\nNo return, one parameter")
-    my_list = list(range(10))
-    print(list_shuffle(my_list), my_list)
+    print("\n == No return, one parameter == ")
+    print('Param:', param, 'Return:', print_random_number_0_x(param))
+
+    # No return no parameter
+    print("\n == No return, no parameter == ")
+    print('Return:', print_random_number_0_9())
+
+    # Return, with one parameter
+    print("\n == Return, with one parameter == ")
+    print('Param:', param, 'Return:', get_random_number_0_x(param))
 
     # Return, without parameter
-    print("\nReturn, no parameter")
-    print(type(current_time_iso()), current_time_iso())
+    print("\n == Return, without parameter == ")
+    print('Return:', get_random_number_0_9())
 
-    # print('\n# Use default value')
-    # say_hello()
-    #
-    # print('\n# passed value with wrong type')
-    # say_hello(123)
-    #
-    # print('\n# function without return')
-    # returned_value = say_hello('Pierre')
-    # print(returned_value)
-    #
-    # print('\n# variable order inversion')
-    # say_hello_mp(age_user=23, name='toto')
-    # say_hello_mp('toto', 23)
-    #
-    # print('\n# user input')
-    # age = ask_age()
-    # print(f'You are {age} year(s) old')
-    #
-    # print('\n# *args + **kwargs')
-    # args_and_kwargs(1, 3, 5, 6, 7, x=1, y=2, z=3)
-    #
-    # print('\n# Sum')
-    # print(sum_example(1, 3, 5, 6, 7))
+    # Optional parameter
+    print("\n == Optional parameter == ")
+    print('Return:', get_random_number_0_x_optional())
 
-    # exemple_mix_positional('+', 1, 2, 3, 4, 5, 6, 7, 8)
+    # Undefined amount of parameter
+    print("\n == Undefined amount of parameter == ")
+    print('Return:', get_random_value_in_values(1, 'A', 3, 5, 'Z', 7, 9))
+
+    # args kwargs
+    print("\n == Args kwargs == ")
+    print('Return:', args_and_kwargs(1, 2, 3, 4, v1='a', v2='c', v3='x'))
