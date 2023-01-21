b2d80c527ec8a7a0de54139c984614f1706d80c3
Pierre Anken <pierre.anken@proton.me>
2023-01-21 10:07:08 +0100

    function cleanup

--- theory/basics/2_variables_types.py
+++ theory/basics/2_variables_types.py
@@ -1,99 +1,99 @@
-if __name__ == '__main__':
-    # I'm a comment - indentation is important
-    print('I\'m printing inside the terminal')
+# I'm a comment - indentation is important
+print('I\'m printing inside the terminal')
 
-    # Attribute + Method call
-    print('hello world'.capitalize())
+# Attribute + Method call
+print('hello world'.capitalize())
 
-    # Assignation
-    var1 = 123
-    var2 = var1 + 2
-    print(var2)
+# Assignation
+var1 = 123
+var2 = var1 + 2
+print(var2)
 
-    # Multiple assignation
-    x, y, z = "Orange", "Banana", "Cherry"
-    a, b, c = ["Orange", "Banana", "Cherry"]
+# Multiple assignation
+x, y, z = "Orange", "Banana", "Cherry"
+a, b, c = ["Orange", "Banana", "Cherry"]
 
-    # Data type
-    print(' === data type === ')
-    my_string = 'abc'  # my_string = ['a', 'b', 'c']
-    print(my_string[0])
+# Data type
+print(' === data type === ')
+my_string = 'abc'  # my_string = ['a', 'b', 'c']
+print(my_string[0])
 
-    my_multiline_string = ''' 
-    SELECT *
-    FROM animals
-    WHERE age > 10
-    LIMIT 10;
-    '''
+my_multiline_string = ''' 
+SELECT *
+FROM animals
+WHERE age > 10
+LIMIT 10;
+'''
 
-    my_int = 1_000_000
-    my_float = 2.4
-    my_bool = True
+my_int = 1_000_000
+my_float = 2.4
+my_bool = True
 
-    # Casting
-    print(' === casting === ')
-    var3 = '100'
-    print(type(var3))
-    print(int(var3) + 5)
-    print(var3 + str(5))
-    print(5 * 'x')
+# Casting
+print(' === casting === ')
+var3 = '100'
+print(type(var3))
+print(int(var3) + 5)
+print(var3 + str(5))
+print(5 * 'x')
 
-    # Structures
-    print(' === structures === ')
-    my_list = ['a', 1, 'test', 'toto', 1]
-    print(my_list)
+# Structures
+print(' === structures === ')
+my_list = ['a', 1, 'test', 'toto', 1]
+print(my_list)
+print(['abc'] * 5)
 
-    empty_list = []  # list()
+empty_list = []  # list()
 
-    my_list[1] = 'dog'
-    print(my_list)
+my_list[1] = 'dog'
+print(my_list)
 
-    my_list.append('cat')
-    print(my_list)
+my_list.append('cat')
+print(my_list)
 
-    my_list.insert(0, 'bird')
-    print(my_list)
+my_list.insert(0, 'bird')
+print(my_list)
 
-    print(' === tuple === ')
-    my_tuple = ('abc', 1, [1, 2, 3])
-    print(my_tuple[2][0])
+print(' === tuple === ')
+my_tuple = ('abc', 1, [1, 2, 3])
+print(my_tuple[2][0])
 
-    print(' === set === ')
-    # set = no duplicates
-    my_set = {'a', 'a', 'b'}
-    empty_set = set()
-    print(my_set)
-    print(list(set([1, 1, 2, 3, 4])))
+print(' === set === ')
+# set = no duplicates
+my_set = {'a', 'a', 'b'}
+empty_set = set()
+print(my_set)
+print(list(set([1, 1, 2, 3, 4])))
 
-    print(' === range === ')
-    my_range = range(1, 15, 2)  # [2, 4, 6, ..]
-    print(list(my_range))
+print(' === range === ')
+my_range = range(1, 15, 2)  # [2, 4, 6, ..]
+print(list(my_range))
 
-    print(' === dict === ')
-    empty_dict = {}
-    my_dict = {
-        'key1': 'value1',
-        2: [1, 2]
+print(' === dict === ')
+empty_dict = {}
+my_dict = {
+    'key1': 'value1',
+    2: [1, 2]
+}
+
+print(my_dict)
+my_dict.update(
+    {
+        'key1': 11,
+        'key3': 123
     }
+)
+print(my_dict)
 
-    print(my_dict)
-    my_dict.update(
-        {
-            'key1': 11,
-            'key3': 123
-        }
-    )
-    print(my_dict)
+my_dict['key4'] = 'abc'
+print(my_dict)
 
-    my_dict['key4'] = 'abc'
-    print(my_dict)
+del my_dict['key4']
+print(my_dict)
 
-    del my_dict['key4']
-    print(my_dict)
+print('key5', my_dict.get('key5', 'Nothing'))  # None
+print('key1', my_dict.get('key1', 'Nothing2'))  # None
 
-    print('key5', my_dict.get('key5', 'Nothing'))  # None
-    print('key1', my_dict.get('key1', 'Nothing2'))  # None
+# print(my_dict['key5'])
 
-    # print(my_dict['key5'])
-
-    print(list(my_dict.keys()))
+print(list(my_dict.keys()))
--- theory/basics/8_functions.py
+++ theory/basics/8_functions.py
@@ -1,3 +1,7 @@
+import datetime
+import random
+
+
 def say_hello(name: str = 'Lucas') -> None:
     """
     Say hello to with a given name
@@ -49,29 +53,68 @@ def exemple_mix_positional(operation: str, *numbers):
         print(f'Operation {operation} non supportée')
 
 
+
+
+def list_shuffle(some_list: list) -> None:
+    """
+    This function shuffle a given list
+    """
+    # if some_list is not None and not empty
+    if some_list:
+        random.shuffle(some_list)
+
+
+def current_time() -> None:
+    """
+    This function print the current date time
+    """
+    print(datetime.datetime.now().isoformat())
+
+
+def current_time_iso() -> str:
+    """
+    This function return the current date time
+    """
+    return datetime.datetime.now().isoformat()
+
+
 if __name__ == '__main__':
-    print('\n# Use default value')
-    say_hello()
 
-    print('\n# passed value with wrong type')
-    say_hello(123)
+    # No return no parameter
+    print("\nNo return no parameter")
+    print(current_time())
 
-    print('\n# function without return')
-    returned_value = say_hello('Pierre')
-    print(returned_value)
+    # No return, one parameter
+    print("\nNo return, one parameter")
+    my_list = list(range(10))
+    print(list_shuffle(my_list), my_list)
 
-    print('\n# variable order inversion')
-    say_hello_mp(age_user=23, name='toto')
-    say_hello_mp('toto', 23)
+    # Return, without parameter
+    print("\nReturn, no parameter")
+    print(type(current_time_iso()), current_time_iso())
 
-    print('\n# user input')
-    age = ask_age()
-    print(f'You are {age} year(s) old')
+    # print('\n# Use default value')
+    # say_hello()
+    #
+    # print('\n# passed value with wrong type')
+    # say_hello(123)
+    #
+    # print('\n# function without return')
+    # returned_value = say_hello('Pierre')
+    # print(returned_value)
+    #
+    # print('\n# variable order inversion')
+    # say_hello_mp(age_user=23, name='toto')
+    # say_hello_mp('toto', 23)
+    #
+    # print('\n# user input')
+    # age = ask_age()
+    # print(f'You are {age} year(s) old')
+    #
+    # print('\n# *args + **kwargs')
+    # args_and_kwargs(1, 3, 5, 6, 7, x=1, y=2, z=3)
+    #
+    # print('\n# Sum')
+    # print(sum_example(1, 3, 5, 6, 7))
 
-    print('\n# *args + **kwargs')
-    args_and_kwargs(1, 3, 5, 6, 7, x=1, y=2, z=3)
-
-    print('\n# Sum')
-    print(sum_example(1, 3, 5, 6, 7))
-
-    exemple_mix_positional('+', 1, 2, 3, 4, 5, 6, 7, 8)
+    # exemple_mix_positional('+', 1, 2, 3, 4, 5, 6, 7, 8)
