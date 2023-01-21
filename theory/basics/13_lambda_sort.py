c4ee0dd661420cc8a48826a4544193c8efc73046
Pierre Anken <pierre.anken@proton.me>
2023-01-21 11:26:43 +0100

    clean lambda

--- theory/basics/13_lambda_sort.py
+++ theory/basics/13_lambda_sort.py
@@ -1,20 +1,19 @@
 import random
 from string import ascii_lowercase as alphabet
 
-word_list = ['table', ' you ', 'castle', 'edge ']
+# lambda functions are inline short functions
+# applied to a set of element
 
-print('Alphabetic sort', sorted(word_list))
-
-print('Sort by length', sorted(word_list, key=len))
+word_list = ['table', ' zen   ', 'castle', 'edge']
 
 
 def len_without_space(value: str):
-    return value.strip()
+    return len(value.strip())
 
 
 print('Sort by length without space', sorted(word_list, key=len_without_space))
 
-print('Sort by length without space, with lambda', sorted(word_list, key=lambda word: word.strip()))
+print('Sort by length without space, with lambda', sorted(word_list, key=lambda word: len(word.strip())))
 
 print('Sort by last letter without space, with lambda', sorted(word_list, key=lambda word: word.strip()[-1]))
 
