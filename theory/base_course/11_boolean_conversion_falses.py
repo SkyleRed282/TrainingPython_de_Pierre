9bc600c819777f0749d6bae92a19a4eb7952f6ba
Pierre Anken <pierre.anken@proton.me>
2023-02-08 16:56:32 +0100

    refactore

--- theory/base_course/11_boolean_conversion_falses.py
+++ theory/base_course/11_boolean_conversion_falses.py
@@ -1,7 +1,7 @@
-false_values = [0, '', False, None, [], {}, tuple(), set(), 0.0]
+false_values = [0, ' ', False, None, [], {}, tuple(), set(), 0.0]
 
 for false_value in false_values:
-    print(false_value, 'of type', type(false_value).__name__, ' =>', bool(false_value))
+    print(f'>{false_value}<', 'of type', type(false_value).__name__, ' =>', bool(false_value))
 
 my_list = []  # False
 if my_list: # if my_list != False and my_list != None and len(my_list) > 0
