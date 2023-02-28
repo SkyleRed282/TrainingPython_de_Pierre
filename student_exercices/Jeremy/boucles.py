a8fff97b229002e87ab44e7084d8b305bf8de1bf
Pierre Anken <pierre.anken@proton.me>
2023-02-28 19:35:09 +0100

    scope

---
+++ student_exercices/Jeremy/boucles.py
@@ -0,0 +1,14 @@
+for x in range(3):
+    print(x)
+
+conteur = 0
+while conteur < 3:
+    print(conteur)
+    conteur += 1
+
+liste1 = ['a', 34, 'cde']
+for valeur in liste1:
+    print(valeur)
+
+for valeur in 'abcd':
+    print(valeur)
---
+++ theory/base_course/14_variable_scope.py
@@ -0,0 +1,16 @@
+my_global = 'globale'
+
+
+def my_function():
+    print(my_global)
+    my_locale = 'locale'
+    global my_globale_2
+    my_globale_2 = 'globale 2'
+
+my_function()
+
+# bug => print(my_locale)
+print(my_globale_2)
+
+
+
