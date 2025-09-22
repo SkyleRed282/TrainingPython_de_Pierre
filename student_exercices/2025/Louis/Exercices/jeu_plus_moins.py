eb340a9275e9d8e6b0e92427f230003e7f6c585c
SkyleRed282 <Louisr.nicolas@gmail.com>
2025-09-22 11:15:41 +0200

    j'ai esseyé d'ajouter le système de choix

--- student_exercices/2025/Louis/Exercices/jeu_plus_moins.py
+++ student_exercices/2025/Louis/Exercices/jeu_plus_moins.py
@@ -11,31 +11,33 @@ while nombre_choisi != nombre_mystere:
     # Demander un nombre
     nombre_choisi_str = input('Entrer un nombre entre 1 et 100: ')
     if not nombre_choisi_str.isdigit():
-        print(f'{nombre_choisi_str} n\'est pas un nombre valable.')
+        print(f'{nombre_choisi_str} n\'est pas un nombre valable.\n')
         continue
 
     nombre_choisi = int(nombre_choisi_str)
 
     # Dire si il est > ou <
     if nombre_choisi < nombre_mystere:
-        print('Le nombre mystère est plus grand')
+        print('Le nombre mystère est plus grand\n')
     elif nombre_choisi > nombre_mystere:
-        print('Le nombre mystère est plus petit')
+        print('Le nombre mystère est plus petit\n')
 
-print('\nBravo tu as trouvé le nombre mystère!')
+print('\nBravo tu as trouvé le nombre mystère!\n')
 
 # poser la question
 
 recommencer_une_partie = 0
 
 
-while recommencer_une_partie != 1 or 2:
+while recommencer_une_partie != "oui" or "non":
     # veut-il recommencer ?
-    réponse_par_oui_ou_non = input('\nVoulez-vous recommencer une partie ?\n(répondez par "oui" ou "non") :')
+    reponse_par_oui_ou_non = input('Voulez-vous recommencer une partie ?\n(répondez par "oui" ou "non" lettre pour lettre) : ')
+# Demander "oui" ou "non"
+    if reponse_par_oui_ou_non != "oui" or "non":
+        print(f'\n{reponse_par_oui_ou_non} n\'est pas lettre pour lettre "oui" ou "non" !\nveuillez recommencer\n')
+        continue
 
-    # il dis n'importe quoi
-
-
+    reponse_par_oui_ou_non = str(reponse_par_oui_ou_non_str)
     # il dis oui
 
 
