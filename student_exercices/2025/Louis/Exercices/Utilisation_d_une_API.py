e7e6e66981d374b16004ff2364e3d8b747103948
SkyleRed282 <Louisr.nicolas@gmail.com>
2025-11-06 19:09:45 +0100

    j ai reessaye de regler le probleme lie a l impossibiliter d entrer un nom

---
+++ student_exercices/2025/Louis/Exercices/Utilisation_d_une_API.py
@@ -0,0 +1,20 @@
+import requests
+
+# 1) Trouver une API publique
+
+name = None
+while name in None:
+    name = input('Donnez un nom : ')
+
+
+base_url = ("https://api.agify.io?name=")
+
+# 2) Executer plusieurs requêtes dessus avec des paramètres
+
+resp = requests.get(f'{base_url}{name}')
+answer = resp.json()
+
+del answer['count']
+
+for key, value in answer.items():
+ print(f'{key.title()}: {value}')
