d283de45501ca2ca41eb388fb7e4bdf67ca914f7
SkyleRed282 <Louisr.nicolas@gmail.com>
2025-12-01 14:48:35 +0100

    j'ai fait un reajustement de complexite de script

--- student_exercices/2025/Louis/Exercices/Test_de_creation_de_fichier.py
+++ student_exercices/2025/Louis/Exercices/Test_de_creation_de_fichier.py
@@ -1,19 +1,30 @@
-# Ce script va faire des générations de "donjons" en caractères spéciaux
+import os
+import time
+# Ce script va donner le nom de chaque moment de la journée par rapport à l'heure
 
-# 1) On demande à l'utilisateur s'il veut générer un donjon
+# 1) On demande à l'utilisateur s'il veut avoir le nom de chaque moment de la journée
 
-generer_ou_pas = None
-while generer_ou_pas not in ("oui", "non") :
-    generer_ou_pas = input('Voulez-vous générer un donjon ?\n(répondez par "oui" ou "non") : ')
+avoir_les_noms_ou_pas = None
+while avoir_les_noms_ou_pas not in ("oui", "non") :
+    avoir_les_noms_ou_pas = input('Voulez-vous avoir le nom de chaque moment de la journée ?\n(répondez par "oui" ou "non") : ')
 
-    if generer_ou_pas not in ("oui", "non"):
-        print(f'{generer_ou_pas} n\'est pas une réponse valable.\n')
+    if avoir_les_noms_ou_pas not in ("oui", "non"):
+        print(f'{avoir_les_noms_ou_pas} n\'est pas une réponse valable.\n')
 
 # 2) exécuter la suite du script selon le choix de l'utilisateur
 
-if generer_ou_pas == "oui" :
+if avoir_les_noms_ou_pas == "oui" :
+# donner le nom de chaque moment de la journée
 
+    if not os.path.exists('Journée'):
+        os.mkdir(path='Journée')
 
+    for hours_time in range(1, 25):
+        if not os.path.exists(f'Journée/{hours_time}'):
+            os.mkdir(path=f'Journée/{hours_time}')
+    for morning_hours in range(2, 12):
+        with open(f'journée/{morning_hours}/Morning.txt', 'w'):
+            pass
 else:
 # demander à l'utilisateur s'il veut clean les fichiers et arrêter le script ou simplement arrêter le script
     clean_les_files_ou_arreter_le_script = None
@@ -23,3 +34,18 @@ else:
 
         if clean_les_files_ou_arreter_le_script not in ("nettoyer et arrêter", "arrêter"):
             print(f'{clean_les_files_ou_arreter_le_script} n\'est pas une réponse valable.\n')
+
+    if clean_les_files_ou_arreter_le_script == "nettoyer et arrêter":
+        if os.path.exists('Journée'):
+            for hours_time in range(1, 25):
+                os.rmdir(path=f'Journée/{hours_time}')
+                os.rmdir(path='journée')
+        else:
+            print("il a aucun fichier à nettoyer")
+
+    else:
+        print("T'avais vraiment besoin de démarrer le script ?")
+        time.sleep(4)
+        print("bon...")
+        time.sleep(1)
+        print("d'accord")
